ac6183a39f9375759dc06f82d6e87d32cb19c7b7
reiji-1999 <68744130+reiji-1999@users.noreply.github.com>
2020-07-31 17:22:29 +0900

    roomsテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "messages#index"
-  resources :user, only: [:edit, :update]
+  resources :users, only: [:edit, :update]
 end
